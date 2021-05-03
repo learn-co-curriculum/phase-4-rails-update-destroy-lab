f824c31a789aa48b0ee0c91682341e6f5ea14b27
ihollander <ianholla@gmail.com>
2021-05-03 18:08:42 -0400

    Added solution

--- plantsy-api/app/controllers/plants_controller.rb
+++ plantsy-api/app/controllers/plants_controller.rb
@@ -18,6 +18,20 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
