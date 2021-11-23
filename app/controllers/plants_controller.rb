e87f8c4a4c1a2b3b9f5841e99f5e9bfb47250fd1
Zachary <tokyo@DESKTOP-9HDMHK0.localdomain>
2021-11-23 12:04:34 -0600

    completed update and destroy

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :error_handle
 
   # GET /plants
   def index
@@ -8,7 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +19,32 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # Update /plant/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+    
+  end
+
+  # Destroy
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end 
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end 
+
+  def error_handle
+    render json: { error: "Plant not found" }, status: :not_found
+  end 
 end
